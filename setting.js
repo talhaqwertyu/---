786f380c60512856208b45c7f3e99fedaafe4b3b
talhaqwertyu <irfan20op3@gmail.com>
2026-01-20 18:06:18 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 ＳＩＬＶＥＲ-ＭＤ 𓆪',
+  author: '‎',
+  botName: "𓆩 ＳＩＬＶＥＲ-ＭＤ 𓆪",
+  botOwner: 'SILVER', // Your name
+  ownerNumber: '923104609886', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/talhaqwertyu/---",
+};
+
+module.exports = settings;
